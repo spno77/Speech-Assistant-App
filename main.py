c9ce6234fe4bab5841523437f49eeeccbcd8d941
spno77 <pilonoti@yahoo.com>
2019-12-29 16:35:42 +0200

    added if statement for serach

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import speech_recognition as sr  
+import webbrowser
 from time import ctime
 
 r = sr.Recognizer()
 
-def record_audio():
+def record_audio(ask = False):
 	with sr.Microphone() as source:
+		if ask:
+			print(ask)
 		r.adjust_for_ambient_noise(source)
 		audio = r.listen(source,timeout=3,phrase_time_limit=3)
 		voice_data = " "
@@ -17,13 +20,16 @@ def record_audio():
 		return voice_data
 
 
-
 def respond(voice_data):
 	if 'what is your name' in voice_data:
 		print('my name is alexis')
 	if 'what time is it' in voice_data:
 		print(ctime())
-
+	if 'search' in voice_data:
+		search = record_audio('What do you want to search for')
+		url = 'https://google.com/search?q=' + search
+		webbrowser.get().open(url)
+		print('Here is what i found for ' + search)
 
 print("How can i help you?")
 voice_data = record_audio()
